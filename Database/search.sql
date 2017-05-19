3e46edf7b9dffdfd20775198c6500819ad588709
EmAiShoaib <emaishoaib@gmail.com>
2017-05-19 23:08:22 +0200

    Revert "Enabled Auto_Increment + Added 'isIndexed' Column"
    This reverts commit af0df41fb810a90d7b2d294c45528ef961d649b6.

--- Database/search.sql
+++ Database/search.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2017 at 09:36 PM
+-- Generation Time: May 19, 2017 at 08:35 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -31,8 +31,7 @@ CREATE TABLE `doc_links` (
   `docID` int(11) NOT NULL,
   `docTitle` varchar(255) DEFAULT 'None Available',
   `docLink` varchar(255) NOT NULL,
-  `docHits` int(11) NOT NULL DEFAULT '1',
-  `isIndexed` int(11) NOT NULL DEFAULT '0'
+  `docHits` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -164,15 +163,6 @@ ALTER TABLE `tag_index`
   ADD PRIMARY KEY (`term`,`docID`),
   ADD KEY `docID` (`docID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `doc_links`
---
-ALTER TABLE `doc_links`
-  MODIFY `docID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
