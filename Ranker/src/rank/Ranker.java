3629e015933468f69fb69e120012048347c46571
EmAiShoaib <emaishoaib@gmail.com>
2017-05-20 22:06:23 +0200

    Added Port Comm. to Ranker

--- Ranker/src/rank/Ranker.java
+++ Ranker/src/rank/Ranker.java
@@ -17,9 +17,11 @@ import org.apache.commons.lang3.*;
 
 import org.tartarus.snowball.ext.*;
 
-public class Ranker {
+public class Ranker 
+{
+	private static String htmls_folder = "C:\\xampp\\htdocs\\MAMgo\\_crawler\\HTMLs\\";
 	
-	public static double[][] tf_idf( ResultSet tag, List<String> queryArray) throws SQLException
+	private static double[][] tf_idf( ResultSet tag, List<String> queryArray) throws SQLException
 	{
 		
 		double[][] tf_idf = new double[queryArray.size()][3];
@@ -54,16 +56,13 @@ public class Ranker {
 					
 					break;
 				}
-				
 			}
-			
-			
 		}
 		
 		return tf_idf;
 	}
 	
-	public static double[][] tf_idf_2( List<String> doc, String query) throws SQLException
+	private static double[][] tf_idf_2( List<String> doc, String query) throws SQLException
 	{
 		
 		double[][] tf_idf = new double[1][3];
@@ -87,35 +86,11 @@ public class Ranker {
 			
 			else if(i == 2)
 				
-				tf_idf[1][0] = tf_idf[1][0] + 5 * StringUtils.countMatches(temp, query);
-			
-			else if(i == 3)
-				
-				tf_idf[1][0] = tf_idf[1][0] + 5 * StringUtils.countMatches(temp, query);
-			
-			else if(i == 4)
-				
-				tf_idf[1][0] = tf_idf[1][0] + 5 * StringUtils.countMatches(temp, query);
-			
-			else if(i == 5)
-				
-				tf_idf[1][0] = tf_idf[1][0] + 5 * StringUtils.countMatches(temp, query);
-			
-			else if(i == 6)
-				
-				tf_idf[1][0] = tf_idf[1][0] + 5 * StringUtils.countMatches(temp, query);
-			
-			else if(i == 7)
-				
-				tf_idf[1][0] = tf_idf[1][0] + 1 * StringUtils.countMatches(temp, query);
-			
-			else if(i == 8)
-				
 				tf_idf[1][0] = tf_idf[1][0] + 1 * StringUtils.countMatches(temp, query);
 			
 			if(StringUtils.countMatches(temp, query)>0)
-				
 				tf_idf[1][1] = 1;
+			
 			i++;
 			
 			
@@ -127,246 +102,326 @@ public class Ranker {
 		return tf_idf;
 	}
 	
-	public static List<String> phrase(ResultSet res)
+	private static List<String> phrase(ResultSet res) throws SQLException, IOException
 	{
-		Document doc = Jsoup.parse("<!DOCTYPE html>"
-        		+ "<html>"
-                + "<title>The Zoo Zoo Zoo Zoo Zoo</title>"
-                + "<h1>Animal Kingdom: Cheetah in Zoo</h1>"
-                + "<p>Many <a href = 'wikipedia.org/cheetah'>cheetahs</a> are taken care of here</p>"
-                + "<p>The zoo also harbors various other animals, and not only cheetah and they are very great.</p>"
-                + "</html>");
-		
+		//Parsing the doc's corresponding HTML file
+		File file = new File(htmls_folder + res.getInt("docID") + ".html");
+        Document doc = Jsoup.parse(file, "UTF-8");
+        
 		Elements title = doc.select("title");
-        Elements p = doc.select("p");
-        Elements a = doc.select("a");
-        Elements h1 = doc.select("h1");
-        Elements h2 = doc.select("h2");
-        Elements h3 = doc.select("h3");
-        Elements h4 = doc.select("h4");
-        Elements h5 = doc.select("h5");
-        Elements h6 = doc.select("h6");
+        Elements h = doc.select("h1, h2, h3, h4, h5, h6");
+        
+        //Extracting the content of the HTML using <html> tag
+        Elements contentAll = doc.select("html");
+
+		//Removing <script> and <style> tags from the content of the HTML
+        contentAll.select("script").remove();
+        contentAll.select("style").remove();
+        
+		//Assuming there is always one <html> tag, thus of interest is only first one
+		Element content = contentAll.first();
 
         //Extracting the string from Elements for each tag
         String titleStr = title.text();
-        String pStr = p.text();
-        String aStr = a.text();
-        String h1Str = h1.text();
-        String h2Str = h2.text();
-        String h3Str = h3.text();
-        String h4Str = h4.text();
-        String h5Str = h5.text();
-        String h6Str = h6.text();
+        String hStr = h.text();
+        String contentStr = content.text();
 
         //Cleaning the string by replacing everything 
         //that is not a word character (a-1, 0-9, _); all what would be left is spaces
         //between each word
         titleStr = titleStr.replaceAll("[^\\w\\s]",""); 
-        pStr = pStr.replaceAll("[^\\w\\s]",""); 
-        aStr = aStr.replaceAll("[^\\w\\s]",""); 
-        h1Str = h1Str.replaceAll("[^\\w\\s]","");
-        h2Str = h2Str.replaceAll("[^\\w\\s]","");
-        h3Str = h3Str.replaceAll("[^\\w\\s]","");
-        h4Str = h4Str.replaceAll("[^\\w\\s]","");
-        h5Str = h5Str.replaceAll("[^\\w\\s]","");
-        h6Str = h6Str.replaceAll("[^\\w\\s]","");
+        hStr = hStr.replaceAll("[^\\w\\s]","");
+        contentStr = contentStr.replaceAll("[^\\w\\s]","");
         
     	List <String> docString = new ArrayList<String>();
         
         docString.add(titleStr);
-        docString.add(h1Str);
-        docString.add(h2Str);
-        docString.add(h3Str);
-        docString.add(h4Str);
-        docString.add(h5Str);
-        docString.add(h6Str);
-        docString.add(pStr);
-        docString.add(aStr);
+        docString.add(hStr);
+        docString.add(contentStr);
         
 		return docString;
 	}
 	
-	public static void main(String[] args) 
-	{	
-	String query="";
-	String url = "jdbc:mysql://localhost:3306/";
-	String user = "root";
-	String password = "";
-	
-	try{
-		
-	//Connecting to the database
-	Class.forName("com.mysql.jdbc.Driver").newInstance();
-	Connection con = DriverManager.getConnection(url, user, password);
-	
-	Statement stt = con.createStatement();
-	stt.execute("USE search");
-	
-	ResultSet res;
-	ResultSet Count;
-	ResultSet Total;
-	
-	//Reading last recorded docID in database
-	res = stt.executeQuery("SELECT docID FROM results");
-	Count = stt.executeQuery("SELECT COUNT(docID) FROM results");
-	Total = stt.executeQuery("SELECT COUNT(docID) FROM doc_links");
-	int countRow = Count.getInt(0);
-	int TCount = Total.getInt(0);
-	int type=0;
-	List <String> queryArray = null;
-	List <String> docString = null;
-	
-	if(!query.startsWith("\"") && !query.endsWith("\""))
+	private static boolean rankDocs(String queryReceived)
 	{
-		type=1;
+		String query = queryReceived;
+		String url = "jdbc:mysql://localhost:3306/";
+		String user = "root";
+		String password = "";
 		
-		queryArray = new ArrayList<String> (Arrays.asList(query.split(" +")));
-		PorterStemmer portStem = new PorterStemmer();
-		int length = queryArray.size();
+		try{
+			
+		//Connecting to the database
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection con = DriverManager.getConnection(url, user, password);
 		
-		for(int i = 0; i < length; i++)
+		Statement stt = con.createStatement();
+		stt.execute("USE search");
+		
+		ResultSet res;
+		ResultSet Count;
+		ResultSet Total;
+		
+		//Reading last recorded docID in database
+		res = stt.executeQuery("SELECT docID FROM results");
+		Count = stt.executeQuery("SELECT COUNT(docID) FROM results");
+		Total = stt.executeQuery("SELECT COUNT(docID) FROM doc_links");
+		int countRow = Count.getInt(0);
+		int TCount = Total.getInt(0);
+		int type=0;
+		List <String> queryArray = null;
+		List <String> docString = null;
+		
+		if(!query.startsWith("\"") && !query.endsWith("\""))
 		{
-			String termStem = "";
-			portStem.setCurrent(queryArray.get(i));
-			portStem.stem();
-			termStem = portStem.getCurrent();
-			if(!queryArray.get(i).equalsIgnoreCase(termStem))
+			type=1;
+			
+			queryArray = new ArrayList<String> (Arrays.asList(query.split(" +")));
+			PorterStemmer portStem = new PorterStemmer();
+			int length = queryArray.size();
+			
+			for(int i = 0; i < length; i++)
 			{
-				queryArray.add(termStem);
-			}
-		}
-	}
-	else
-	{
-		type=2;
-		
-	}
-	
-	double[][][] my_results_tf = new double[countRow][queryArray.size()][3];
-	double[][][] my_results_tf_phrase = new double[countRow][1][3];
-		
-			
-	//while (res.next())
-		for(int i = 0; i < countRow; i++)
-		{
-			ResultSet tag;
-			tag = stt.executeQuery("SELECT * FROM tag_index WHERE docID=" +res.getInt("docID"));
-			res.next();
-			if(type == 1)
-				
-			{
-				
-				double[][] temp = new double[queryArray.size()][2];
-				temp = tf_idf( tag , queryArray );
-				my_results_tf[i]=temp;
-					
-			}
-			
-			else if(type==2)
-			{
-				docString=phrase(res);
-				double[][] temp = new double[1][2];
-				temp = tf_idf_2( docString , query );
-				my_results_tf_phrase[i]=temp;
-					
-					
-			}
-			
-			
-			
-		}
-		
-		
-			if(type == 1)
-				
-			{
-				for(int i = 0; i <  queryArray.size(); i++)
+				String termStem = "";
+				portStem.setCurrent(queryArray.get(i));
+				portStem.stem();
+				termStem = portStem.getCurrent();
+				if(!queryArray.get(i).equalsIgnoreCase(termStem))
 				{
-					double[] idf= new double[queryArray.size()];
-						
-					for(int j = 0; j < countRow; j++)
-					{
-						
-						idf[i] = idf[i] + my_results_tf[j][i][1];
-						my_results_tf[j][i][2] = 0;
-					}	
+					queryArray.add(termStem);
+				}
+			}
+		}
+		else
+		{
+			type=2;
+			
+		}
+		
+		double[][][] my_results_tf = new double[countRow][queryArray.size()][3];
+		double[][][] my_results_tf_phrase = new double[countRow][1][3];
+			
+				
+		//while (res.next())
+			for(int i = 0; i < countRow; i++)
+			{
+				ResultSet tag;
+				tag = stt.executeQuery("SELECT * FROM tag_index WHERE docID=" +res.getInt("docID"));
+				res.next();
+				if(type == 1)
 					
+				{
+					
+					double[][] temp = new double[queryArray.size()][2];
+					temp = tf_idf( tag , queryArray );
+					my_results_tf[i]=temp;
+						
 				}
 				
-				for(int i = 0; i <  queryArray.size(); i++)
+				else if(type==2)
 				{
-					double[] idf= new double[queryArray.size()];
+					docString=phrase(res);
+					double[][] temp = new double[1][2];
+					temp = tf_idf_2( docString , query );
+					my_results_tf_phrase[i]=temp;
 						
-					for(int j = 0; j < countRow; j++)
+						
+				}
+				
+				
+				
+			}
+			
+			
+				if(type == 1)
+					
+				{
+					for(int i = 0; i <  queryArray.size(); i++)
 					{
-						if(idf[i]>0)
+						double[] idf= new double[queryArray.size()];
+							
+						for(int j = 0; j < countRow; j++)
 						{
-							my_results_tf[j][i][1] = TCount/ idf[i];
-							my_results_tf[j][i][2] = my_results_tf[j][i][0] * my_results_tf[j][i][1];
-						}
-					}	
+							
+							idf[i] = idf[i] + my_results_tf[j][i][1];
+							my_results_tf[j][i][2] = 0;
+						}	
+						
+					}
 					
-				}
-				ResultSet send=stt.executeQuery("SELECT docID FROM results");
-				for(int i = 0; i <  countRow; i++)
-				{
-					
-					double total = 0;
-					
-					for(int j = 0; j < queryArray.size(); j++)
+					for(int i = 0; i <  queryArray.size(); i++)
 					{
-						total = total + my_results_tf[i][j][2];	
-					}	
-					stt.executeQuery("UPDATE results SET docRank = " + total + " WHERE docID = " + send.getInt("docID"));
-					send.next();
+						double[] idf= new double[queryArray.size()];
+							
+						for(int j = 0; j < countRow; j++)
+						{
+							if(idf[i]>0)
+							{
+								my_results_tf[j][i][1] = TCount/ idf[i];
+								my_results_tf[j][i][2] = my_results_tf[j][i][0] * my_results_tf[j][i][1];
+							}
+						}	
+						
+					}
+					ResultSet send=stt.executeQuery("SELECT docID FROM results");
+					for(int i = 0; i <  countRow; i++)
+					{
+						
+						double total = 0;
+						
+						for(int j = 0; j < queryArray.size(); j++)
+						{
+							total = total + my_results_tf[i][j][2];	
+						}	
+						stt.executeQuery("UPDATE results SET docRank = " + total + " WHERE docID = " + send.getInt("docID"));
+						send.next();
+					}
+					
+					
+					
 				}
 				
-				
-				
-			}
-			
-			else if(type==2)
-			{
-				double idf= 0;
-				
-				for(int j = 0; j < countRow; j++)
+				else if(type==2)
 				{
-					idf = idf + my_results_tf_phrase[j][0][1];
-					my_results_tf_phrase[j][0][2] = 0;
-				}
-				
-				if(idf>0)
-				{
-				
+					double idf= 0;
+					
 					for(int j = 0; j < countRow; j++)
 					{
-						my_results_tf_phrase[j][0][1] = TCount / idf;
-						my_results_tf_phrase[j][0][2] = my_results_tf_phrase[j][0][0] * my_results_tf_phrase[j][0][1];
+						idf = idf + my_results_tf_phrase[j][0][1];
+						my_results_tf_phrase[j][0][2] = 0;
 					}
-				}
-				
-				ResultSet send=stt.executeQuery("SELECT docID FROM results");
-				for(int i = 0; i <  countRow; i++)
-				{
 					
-					double total = my_results_tf[i][0][2];
-					stt.executeQuery("UPDATE results SET docRank = " + total + " WHERE docID = " + send.getInt("docID"));
-					send.next();
-				}
-				
+					if(idf>0)
+					{
 					
+						for(int j = 0; j < countRow; j++)
+						{
+							my_results_tf_phrase[j][0][1] = TCount / idf;
+							my_results_tf_phrase[j][0][2] = my_results_tf_phrase[j][0][0] * my_results_tf_phrase[j][0][1];
+						}
+					}
+					
+					ResultSet send=stt.executeQuery("SELECT docID FROM results");
+					for(int i = 0; i <  countRow; i++)
+					{
+						
+						double total = my_results_tf[i][0][2];
+						stt.executeQuery("UPDATE results SET docRank = " + total + " WHERE docID = " + send.getInt("docID"));
+						send.next();
+					}
+					
+						
+				}
+		}
+		
+		catch(Exception error)
+		{	
+			System.out.println(error.getMessage());	
+		}
+		
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		// Declaring ServerSockets
+		ServerSocket serverSocket_r = null;
+		ServerSocket serverSocket_wr = null;
+		
+		// Declaring Sockets
+		Socket socket_r = null;
+		Socket socket_wr = null;
+		
+		String query = null;
+		
+		// Create a new socket at port 1245 (Will be used to read string from PHP)
+		try
+		{
+			serverSocket_r = new ServerSocket(1245);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Could not listen to port 1245");
+			System.exit(-1);
+		}
+		
+		// Create a new socket at port 1246 (Will be used to send string to PHP)
+		try
+		{
+			serverSocket_wr = new ServerSocket(1246);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Could not listen to port 1246");
+			System.exit(-1);
+		}
+		
+		// Keep running indefinitely (terminates manually)
+		while(true)
+		{
+			// Listen to port 1245 for reading
+			try
+			{
+				socket_r = serverSocket_r.accept();
 			}
-		
-	
-	}
-	
-	catch(Exception error){
-		
-		System.out.println(error.getMessage());
-		
-	}
-	
-		
+			catch (Exception e)
+			{
+				System.out.println("Accept failed");
+				System.exit(-1);
+			}
+			
+			// Read string from port 1245
+			try
+			{
+				InputStreamReader isr = new InputStreamReader(socket_r.getInputStream());
+				BufferedReader br = new BufferedReader(isr);
+				
+				query = br.readLine();
+			}
+			catch (Exception e)
+			{
+				System.out.println("String reading on port from client failed");
+				System.exit(-1);
+			}
+			
+			//Printing in console message indicating permission received from PHP
+			System.out.println("Permission to rank table 'results' received");
+			
+			boolean result = false;
+			String rankerResponse = "";
+			
+			//Execute method to rank the documents in table 'results'
+			if(query != null)
+				result = rankDocs(query);
+			
+			//If result returned true, then ranker worked
+			if (result == true)
+				rankerResponse = "1\n";
+			else
+				rankerResponse = "-1\n";
+			
+			// Listen to port 1246
+			try
+			{
+				socket_wr = serverSocket_wr.accept();
+			}
+			catch (Exception e)
+			{
+				System.out.println("Accept failed");
+				System.exit(-1);
+			}
+						
+			//Write string of a number to port 1246
+			try
+			{
+				PrintStream ps = new PrintStream(socket_wr.getOutputStream());
+				ps.print(rankerResponse);					
+			}
+			catch (Exception e)
+			{
+				System.out.println("String writing on port to client failed");
+				System.exit(-1);
+			}
+		}
 	}
 }
-	
